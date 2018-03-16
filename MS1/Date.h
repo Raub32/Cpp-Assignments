64a21f1cee3971a0a47af81925a90132807900f3
Raub32 <31700264+Raub32@users.noreply.github.com>
2018-03-15 22:07:56 -0400

    edited

--- MS1/Date.h
+++ MS1/Date.h
@@ -2,9 +2,10 @@
 // Final Project Milestone 1
 //
 // Version 1.0
-// Date
-// Author
+// Date: 2018/03/2018
+// Author: Robert Aubow
 // Description
+//
 //
 //
 //
@@ -13,19 +14,24 @@
 // -----------------------------------------------------------
 // Name               Date                 Reason
 /////////////////////////////////////////////////////////////////
+// Error States
+#define NO_ERROR  0;
+#define CIN_FAILED  1;
+#define YEAR_ERROR  2;
+#define MON_ERROR  3;
+#define DAY_ERROR 4;
+#define MAX_YEAR  2030;
+#define MIN_YEAR  2012;
+
 #ifndef AMA_DATE_H
 #define AMA_DATE_H
 
-// Error States
-#define NO_ERROR = 0;
-#define CIN_FAILED = 1;
-#define YEAR_ERROR = 2;
-#define MON_ERROR = 3;
-#define DAY_ERROR = 4;
+#include <iostream>
+
 
 namespace AMA {
 
-	-class Date {
+	class Date {
 		int year;
 		int day_of_month;
 		int month;
@@ -34,11 +40,11 @@ namespace AMA {
 	public:
 		int mdays(int mon, int year)const;
 		void errCode(int errorCode);
-		
+
 		// class constructor
 		Date();
 		Date(int year, int month, int day);
-		
+
 		//operator overloads
 		bool operator==(const Date& rhs) const;
 		bool operator!=(const Date& rhs) const;
@@ -47,13 +53,13 @@ namespace AMA {
 		bool operator<=(const Date& rhs) const;
 		bool operator>=(const Date& rhs) const;
 
-		int errCode() const;
+		int errorCode() const;
 		bool bad() const;
 		std::istream& read(std::istream& istr);
 		std::ostream& write(std::ostream& ostr) const;
-		friend operator<<(const std::ostream& ostr) const;
-		friend operator>>(std::istream& istr);
-
+		friend std::ostream& operator<<(const std::ostream& ostr, const Date & c);
+		friend std::istream& operator>>(std::istream& istr, const Date &c);
 	};
 }
+
 #endif
